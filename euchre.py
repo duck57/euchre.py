100ed1f75d14eefe1fb7ce6fa7fa6d78d6ace75a
Chris J.M <chris@euchre.us>
2020-02-24 23:49:27 -0500

    check for victory at end of game

--- euchre.py
+++ euchre.py
@@ -20,10 +20,10 @@ Notable differences (to match how I learned in high school calculus) include:
 Mothjab is a funny word with no current meaning.
 """
 
-
 from enum import Enum
+from itertools import cycle
 import random
-from typing import List
+from typing import List, Optional, Tuple, Iterable, Iterator, Set
 
 
 class Suit(Enum):
@@ -59,7 +59,7 @@ class Rank(Enum):
     LEFT_BOWER = 15
     RIGHT_BOWER = 16
 
-    def disp_name(self) -> str:
+    def long_display_name(self) -> str:
         if self == self.ACE_HI:
             return "ACE"
         if self == self.ACE_LO:
@@ -74,11 +74,11 @@ class Card:
         self.d_rank = rank
         self.d_suit = suit
 
-    def cardname(self) -> str:
-        return self.rank.disp_name() + " of " + self.suit.plural_name()
+    def card_name(self) -> str:
+        return self.rank.long_display_name() + " of " + self.suit.plural_name()
 
     def __str__(self) -> str:
-        return self.cardname()
+        return self.card_name()
 
 
 # make a euchre deck
@@ -96,19 +96,27 @@ def make_pinochle_deck() -> List[Card]:
 
 
 class Player:
-    hand: List[Card]
+    hand: List[Card]  # sorted
     name: str
+    tricks: int = 0
 
     def __init__(self, name: str):
         self.name = name
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Team:
-    players: List[Player]
-    score: int
+    players: Set[Player]
+    score: int = 0
+    bid: int
 
-    def __init__(self, players: List[Player]):
+    def __init__(self, players: Set[Player]):
         self.players = players
+
+    def __str__(self):
+        return str(self.players)
 
 
 # main method
@@ -117,28 +125,73 @@ def main(handedness: int = 4):
 
 
 # normal euchre would be an entirely different function
-def double_deck(handedness: int = 4):
+def double_deck(handedness: int = 4) -> None:
     deck: List[Card] = make_pinochle_deck()
-    random.shuffle(deck)
-    l: int = len(deck) // handedness  # could be hardcoded
-
     plist: List[Player]
-    tea_lst: List[Team]
+    tea_lst: Set[Team]
+    victory_threshold: int = 50
+    mercy_rule: int = -50
+    bad_ai_end: int = -25
 
     # set up teams and players
     if handedness == 3:
         plist = [Player("One"), Player("Two"), Player("Third")]
-        tea_lst = [Team([plist[0]]), Team([plist[1]]), Team([plist[2]])]
+        tea_lst = {Team({plist[0]}), Team({plist[1]}), Team({plist[2]})}
     else:
         plist = [Player("North"), Player("East"), Player("South"), Player("West")]
-        tea_lst = [Team([plist[0], plist[2]]), Team([plist[1], plist[3]])]
+        tea_lst = {Team({plist[0], plist[2]}), Team({plist[1], plist[3]})}
+    deal_order: Iterator[Player] = cycle(plist)
+    next(deal_order)
+    next(deal_order)  # initial dealer for 4-hands should be South
 
-    idx = 0
-    for player in plist:
-        player.hand = deck[idx:idx+l]
-        print(f"\n{player.name}")
-        for card in player.hand:
-            print(card)
+    def victory_check() -> Tuple[int, Optional[Team]]:
+        scores_all_bad: bool = True
+        for team in tea_lst:
+            if team.score >= victory_threshold:
+                return 1, team
+            if team.score < mercy_rule:
+                return -1, team
+            if team.score > bad_ai_end:
+                scores_all_bad = False
+        if scores_all_bad:
+            return -1, None
+        return 0, None
+
+    # play the game
+    v: Tuple[int, Optional[Team]] = victory_check()
+    while v[0] == 0:
+        dealer: Player = next(deal_order)
+        bid_order: List[Player] = [next(deal_order), next(deal_order), next(deal_order), next(deal_order)]
+        play_hand(deck, handedness, bid_order, tea_lst)
+        v = victory_check()
+
+    # display final scores
+    for t in tea_lst:
+        print(t.score)
+
+
+def play_hand(deck: List[Card], handedness: int, bid_order: List[Player], teams: Set[Team]) -> None:
+    random.shuffle(deck)
+    hand_size: int = len(deck) // handedness  # could be hardcoded
+    ppt: int = len(bid_order) // len(teams)
+    bid_cycle: Iterator[Player] = cycle(bid_order)
+
+    # deal the cards
+    idx: int = 0
+    for player in bid_order:
+        player.hand = deck[idx: idx + hand_size]
+    print(f"Dealer: {bid_order[-1]}")
+
+    # bidding
+
+    # play the tricks
+    while hand_size > 0:
+        # play_trick(trump)  # TODO
+        hand_size -= 1
+
+    # calculate scores
+    for t in teams:
+        t.score += random.randrange(-12, 14)
 
 
 if __name__ == "__main__":
