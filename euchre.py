b4edd8b66f6ff05239eb317c5b72c9a8f60b89b2
Chris J.M <chris@euchre.us>
2020-02-25 20:10:09 -0500

    deal the cards properly this time

--- euchre.py
+++ euchre.py
@@ -21,43 +21,52 @@ Mothjab is a funny word with no current meaning.
 """
 
 from enum import Enum
-from itertools import cycle
+from itertools import cycle, islice
 import random
 from typing import List, Optional, Tuple, Iterable, Iterator, Set
 
 
 class Suit(Enum):
-    JOKER = 0
-    CLUB = 1
-    DIAMOND = 2
-    SPADE = 3
-    HEART = 4
-    TRUMP = 5
+    JOKER = (0, "🃏")
+    CLUB = (1, "♣")
+    DIAMOND = (2, "♢")
+    SPADE = (3, "♠")
+    HEART = (4, "♡")
+    TRUMP = (5, "T")
 
     def plural_name(self) -> str:
         if self != self.TRUMP:
             return self.name + "S"
         return self.name
 
+    def __str__(self):
+        return self.plural_name()
+
+    def __repr__(self):
+        return self._value_[1]
+
+    def value(self):
+        return self._value_[0]
+
 
 class Rank(Enum):
-    JOKER = 0
-    ACE_LO = 1
-    TWO = 2
-    THREE = 3
-    FOUR = 4
-    FIVE = 5
-    SIX = 6
-    SEVEN = 7
-    EIGHT = 8
-    NINE = 9
-    TEN = 10
-    JACK = 11
-    QUEEN = 12
-    KING = 13
-    ACE_HI = 14
-    LEFT_BOWER = 15
-    RIGHT_BOWER = 16
+    JOKER = (0, "🃟")
+    ACE_LO = (1, "a")
+    TWO = (2, "2")
+    THREE = (3, "3")
+    FOUR = (4, "4")
+    FIVE = (5, "5")
+    SIX = (6, "6")
+    SEVEN = (7, "7")
+    EIGHT = (8, "8")
+    NINE = (9, "9")
+    TEN = (10, "🔟")
+    JACK = (11, "J")
+    QUEEN = (12, "Q")
+    KING = (13, "K")
+    ACE_HI = (14, "A")
+    LEFT_BOWER = (15, "L")
+    RIGHT_BOWER = (16, "R")
 
     def long_display_name(self) -> str:
         if self == self.ACE_HI:
@@ -65,6 +74,15 @@ class Rank(Enum):
         if self == self.ACE_LO:
             return "ACE"
         return self.name
+
+    def value(self):
+        return self._value_[0]
+
+    def __repr__(self):
+        return self._value_[1]
+
+    def __str__(self):
+        return self.long_display_name()
 
 
 class Card:
@@ -80,13 +98,16 @@ class Card:
     def __str__(self) -> str:
         return self.card_name()
 
+    def __repr__(self):
+        return f"{repr(self.d_rank)}{repr(self.d_suit)}"
+
 
 # make a euchre deck
 def make_euchre_deck() -> List[Card]:
     deck = []
-    for rank in range(9, 15):
-        for suit in range(1, 5):
-            deck.append(Card(Rank(rank), Suit(suit)))
+    for rank in [Rank.NINE, Rank.TEN, Rank.JACK, Rank.QUEEN, Rank.KING, Rank.ACE_HI]:
+        for suit in [Suit.HEART, Suit.CLUB, Suit.SPADE, Suit.DIAMOND]:
+            deck.append(Card(rank, suit))
     return deck
 
 
@@ -166,12 +187,7 @@ def double_deck(handedness: int = 4) -> None:
     v: Tuple[int, Optional[Team]] = victory_check()
     while v[0] == 0:
         dealer: Player = next(deal_order)
-        bid_order: List[Player] = [
-            next(deal_order),
-            next(deal_order),
-            next(deal_order),
-            next(deal_order),
-        ]
+        bid_order: List[Player] = list(islice(deal_order, handedness))
         play_hand(deck, handedness, bid_order, tea_lst)
         v = victory_check()
 
@@ -190,9 +206,11 @@ def play_hand(
 
     # deal the cards
     idx: int = 0
+    print(f"Dealer: {bid_order[-1]}")
     for player in bid_order:
         player.hand = deck[idx : idx + hand_size]
-    print(f"Dealer: {bid_order[-1]}")
+        idx += hand_size
+        print(f"{player}: {player.hand}")
 
     # bidding
 
